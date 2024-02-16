bf850c0f29b59b32ab121a70f5b63f8669da651f
raghubanshi <praveenraghubanshi2016@gmail.com>
2024-02-16 16:23:52 -0600

    cors error fixed

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://127.0.0.1:5000/api";
 
 /** given data about a cupcake, generate html */
 
@@ -27,5 +27,43 @@ async function showInitialCupcakes() {
         $("#cupcakes-list").append(newCupcake);
     }
 }
+
+
+/** handle form for adding of new cupcakes */
+
+$("#new-cupcake-form").on("submit", async function (evt) {
+  evt.preventDefault();
+
+  let flavor = $("#form-flavor").val();
+  let rating = $("#form-rating").val();
+  let size = $("#form-size").val();
+  let image = $("#form-image").val();
+
+  const newCupcakeResponse = await axios.post(`${BASE_URL}/cupcakes`, {
+    flavor,
+    rating,
+    size,
+    image
+  });
+
+  console.log(newCupcakeResponse.data, 'jkhjkhkj');
+
+  let newCupcake = $(generateCupcakeHTML(newCupcakeResponse.data.cupcake));
+  $("#cupcakes-list").append(newCupcake);
+  $("#new-cupcake-form").trigger("reset");
+});
+
+
+/** handle clicking delete: delete cupcake */
+
+$("#cupcakes-list").on("click", ".delete-button", async function (evt) {
+  evt.preventDefault();
+  let $cupcake = $(evt.target).closest("div");
+  let cupcakeId = $cupcake.attr("data-cupcake-id");
+
+  await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
+  $cupcake.remove();
+});
+
 
 $(showInitialCupcakes);
