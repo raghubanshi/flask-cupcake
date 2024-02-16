31f90b9a75c74d51c24ee219f7eb65e3a3404c68
raghubanshi <praveenraghubanshi2016@gmail.com>
2024-02-16 15:51:07 -0600

    flask-cupcake

---
+++ static/cupcakes.js
@@ -0,0 +1,31 @@
+const BASE_URL = "http://localhost:5000/api";
+
+/** given data about a cupcake, generate html */
+
+function generateCupcakeHTML(cupcake) {
+    return `
+      <div data-cupcake-id=${cupcake.id}>
+        <li>
+          ${cupcake.flavor} / ${cupcake.size} / ${cupcake.rating}
+          <button class="delete-button">X</button>
+        </li>
+        <img class="Cupcake-img"
+              src="${cupcake.image}"
+              alt="(no image provided)">
+      </div>
+    `;
+}
+
+
+/** put initial cupcakes on page. */
+
+async function showInitialCupcakes() {
+    const response = await axios.get(`${BASE_URL}/cupcakes`);
+
+    for (let cupcakeData of response.data.cupcakes) {
+        let newCupcake = $(generateCupcakeHTML(cupcakeData));
+        $("#cupcakes-list").append(newCupcake);
+    }
+}
+
+$(showInitialCupcakes);
